f1c9eb47663a1846b8abd4e31a529b62a45ec7ee
LKanan <lucienkanani@gmail.com>
2025-04-03 09:21:44 +0200

    Add variable declarations and output statement in variables_in_c++.cpp

--- variables_in_c++.cpp
+++ variables_in_c++.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+#include<string>
+
 using namespace std;
 
 int main(){
     // Déclaration de variables
-    
+    int age (20);
+    bool estMajeur (false);
+    char initialCharacter ('A');
+    string nom ("Dupont");
+
+    cout <<"Mr " << nom << " a " << age << " ans" << " et sa majorité est " << estMajeur << endl;
+
     return 0;
 }
